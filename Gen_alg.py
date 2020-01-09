aa38c0317388874f8f2acbb80e0b354e8ec4a829
Jordan Le <jordan.le.official@gmail.com>
2020-01-08 18:30:24 -0800

    Building simple genetic algorithm first for testing.

--- Gen_alg.py
+++ Gen_alg.py
@@ -13,15 +13,39 @@ Genetic algorithm class will manage network tuning.
 """
 class Gen_alg:
     def __init__(self):
-        self.pop_num = 10
+        self.pop_num = 12               # Population Number
+        self.remaining = int(self.pop_num / 2)
+        self.gen_num = 5                                    # Number of generations
         self.mutation_rate = .05
         self.population = [DNA(id) for id in range(self.pop_num)]
+        self.survivors = np.zeros(self.remaining)           # Survivors per generation
 
     def fitness(self):      # Take the top half of the population of them
+        function = [10, 20, 30, 20, 10]                     # Temp example to make sure this works
+        pop_fits = np.zeros(self.pop_num, dtype=int)        # Holds all fitness values
+        pop_ind = 0
+        f2 = np.asarray(function)
+
+        for pop in self.population:                         # Each individual in the population
+            fit = np.where(f2 == pop.hidden_layers)         # Where they match
+            fit_score = len(fit[0])
+            pop.fit_vals.append(fit_score)      # Each population member has a list of their fitnesses
+            pop_fits[pop_ind] = fit_score       # Update the pop's score for specific index
+            pop_ind += 1                        # Increment index
+            print("ID: {}\tFit Score: {}".format(pop.id, fit_score))
+
+        surv_ind = np.argpartition(pop_fits, -self.remaining)[-self.remaining:]     # Index of the survivors
+        print("Survivor indexes: {}\tSurv_fit_vals: {}".format(surv_ind, pop_fits[surv_ind]))
+
+
+
+    def cross_over(self):       # Recombine the existing survivors
         pass
 
-    def cross_over(self):       # Recombine the existing
-        pass
+    def generation(self):       # Run the GA
+        for i in range(self.gen_num):
+            print("Gen: {}".format(i))
+            self.fitness()
 
 
 """
@@ -33,7 +57,7 @@ class DNA:
         self.id = id                        # The populant's id number
         self.fit_vals = []                  # Accuracy scores to be added
         self.input_layer = 15               # Same number for each input
-        self.hidden_layers = [rand.randint(1, 100) for i in range(3)]       # Set various hidden layers randomly
+        self.hidden_layers = [rand.randint(10, 35) for i in range(5)]       # Set various hidden layers randomly
         self.output_layer = 5               # Placeholder output layer
 
 
@@ -47,3 +71,5 @@ if __name__ == "__main__":
     print(GA.pop_num)
     for i in range(len(GA.population)):
         print("Pop_Id: {}\nLayers: {}\n".format(GA.population[i].id, GA.population[i].hidden_layers))
+
+    GA.generation()
